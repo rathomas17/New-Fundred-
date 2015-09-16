919f3e8354b8762f92b2a0864d3ae8ef13fa9d8b
rathomas17 <rathomas17@gmail.com>
2015-09-16 18:00:47 -0400

    map change

--- client/templates/map.js
+++ client/templates/map.js
@@ -65,18 +65,23 @@ Template.google.events ({
   'click': function(event){
     alert('amap clicked', event.target);
     var myLatLng = {lat: 38.9881, lng: -82.0444};
+    var bostonLatLng = {lat: 42.3601, lng:71.0589};
 
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+
+
+  /*  var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
         center: myLatLng
-      });
+      });*/
 
       var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
         title: 'Hello World!'
       });
+
+      
 
   }
 
@@ -96,9 +101,15 @@ Template.google.helpers({
 Template.google.onCreated(function(){
   GoogleMaps.ready('google', function(map){
     // Add a marker to the map once it's ready
+
+
+
+
     var marker = new google.maps.Marker({
       position: map.options.center,
       map: map.instance
+
     });
+
   });
 });
